28af984241d2b7364a8f93a31adfa1d105662cac
lordrichard6 <paulolopesreizinho@gmail.com>
2021-06-30 23:47:30 +0200

    project cards adapted for mobile

--- src/components/projectCards/index.tsx
+++ src/components/projectCards/index.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 import Carousel, { Dots, slidesToShowPlugin } from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
+import { useMediaQuery } from 'react-responsive'
+import { SCREENS } from '../../components/responsive'
 
 import { SingleCard } from './singleCard'
 import { SecondaryTitle } from '../../assets/styles'
@@ -11,9 +13,11 @@ import { projectsInfo } from '../../assets/static'
 export function ProjectCards() {
     const [current, setCurrent] = useState(0)
 
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm })
+
     console.log(projectsInfo)
 
-    const numberOfDots = Math.ceil(projectsInfo.length / 3)
+    const numberOfDots = isMobile ? projectsInfo.length :  Math.ceil(projectsInfo.length / 3)
 
     return (
         <CardContainer>
@@ -30,6 +34,28 @@ export function ProjectCards() {
                         numberOfSlides: 3,
                     }
                 }]}
+                breakpoints={{
+                    640: {
+                        plugins: [
+                            {
+                                resolve: slidesToShowPlugin,
+                                options: {
+                                    numberOfSlides: 1
+                                }
+                            }
+                        ]
+                    },
+                    900: {
+                        plugins: [
+                            {
+                                resolve: slidesToShowPlugin,
+                                options: {
+                                    numberOfSlides: 2
+                                }
+                            }
+                        ]
+                    }
+                }}
             >
                 {projectsInfo.map((n) => {
                     return (
